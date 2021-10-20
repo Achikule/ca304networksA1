1b23987377d7f7f7577d2d5887d8b1e1fa6c07e3
Achikule <achikule279@gmail.com>
2021-10-20 20:20:22 +0100

    v1

--- stdin.py
+++ stdin.py
@@ -1,14 +1,16 @@
 #!usr/bin/env python3
 
-address = "102.121.0.3"
-subnet = "255.255.255.128"
+address = "192.168.0.1"
+subnet = "255.255.255.192"
 asplit = address.split(".")
 sub_split = subnet.split(".")
 binary_sub = list(map(int, sub_split))
 bi = [f"{i:08b}" for i in binary_sub]
 a = []
 j = 0
-
+classA = ["101.221.3.2", "17.82.244.1"]
+classB = ["133.1.45.0", "130.234.7.12"]
+classC = ["192.168.0.1", "199.168.35.33"]
 while j < 4:
     for i in bi[j]:
         a.append(int(i))
@@ -23,7 +25,19 @@ cidr = subnet_iter - 2
 
 copybinary_sub = [i for i in binary_sub if i > 0]
 ind = len(copybinary_sub) - 1
-z = [int(asplit[0])]
+
+if address in classA:
+    z = [asplit[0]]
+    z = list(map(int, z))
+
+elif address in classB:
+    z = asplit[:2]
+    z = list(map(int, z))
+
+else:
+    z = asplit[:3]
+    z = list(map(int, z))
+
 
 blocksize = 256 - int(binary_sub[ind])
 b = [0] * 4
@@ -31,6 +45,7 @@ b = [0] * 4
 for i in range(len(z)):
     b[i] = z[i]
 
+print(b)
 x = binary_sub[ind]
 valid_subnets = []
 if ind == 0:
@@ -39,16 +54,36 @@ if ind == 0:
 
 if ind == 1:
     for i in range(0, x + 1, blocksize):
+
+        if len(z) == 2:
+            str_b = list(map(str, b))
+            valid_subnets.append(".".join(str_b))
+            break
+
         b[ind] = i
         str_b = list(map(str, b))
         valid_subnets.append(".".join(str_b))
 
+
 if ind == 2:
     for n in range(1, 257, 1):
         for i in range(0, x + 1, blocksize):
+
+            if len(z) == 3:
+                str_b = list(map(str, b))
+                valid_subnets.append(".".join(str_b))
+                break
+
             b[ind] = i
             str_b = list(map(str, b))
             valid_subnets.append(".".join(str_b))
+
+        if len(z) == 2:
+            break
+
+        if len(z) == 3:
+            break
+
         i = 0
         b[1] = n
 
@@ -59,12 +94,22 @@ if ind == 3:
                 b[ind] = i
                 str_b = list(map(str, b))
                 valid_subnets.append(".".join(str_b))
+
+            if len(z) == 3:
+                break
+
             i = 0
             b[2] = n
+
+        if len(z) == 3:
+            break
+        if len(z) == 2:
+            break
+
         n = 0
         b[1] = m
 
 
 
-print(bi, sum(a), cidr, blocksize, ind, copybinary_sub, len(valid_subnets), "\n", valid_subnets[-50:], "\n", first_addresses, "\n", broadcast_addresses, "\n",
+print(bi, sum(a), cidr, blocksize, ind, copybinary_sub, z, len(valid_subnets), "\n", valid_subnets[-50:], "\n", first_addresses, "\n", broadcast_addresses, "\n",
       last_addresses)
